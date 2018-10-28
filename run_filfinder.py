68788829b3a37287d1a5408f67460f20c615fc0b
Remy Indebetouw <rindebet@nrao.edu>
2018-10-28 11:38:19 -0400

    run_filfinder runs filfinder and returns the fils structure; uses RI forked version of filfinder with some bug fixes

---
+++ run_filfinder.py
@@ -0,0 +1,140 @@
+from astropy.io import fits
+from astropy import units as u
+import sys, os, pickle
+
+# this version switches to filfinder2D (from filfind_class::fil_finder_2D)
+
+gitpaths=['/Users/remy/lustre/naasc/users/rindebet/github/lmc-alma-analysis/',
+          '/Users/remy/lustre/naasc/users/rindebet/github/FilFinder/',
+          '/Users/remy/lustre/naasc/users/rindebet/github/FilFinder/fil_finder']
+for gitpath in gitpaths:
+    if not gitpath in sys.path:
+        sys.path.insert(0,gitpath)
+
+from fil_finder import FilFinder2D
+import pylab as pl
+pl.ion()
+import numpy as np
+from scipy.signal import savgol_filter 
+import pdb 
+
+def run_filfinder(label='mycloud', mom8file=None, mom0file=None, redo=False,
+                  distpc=4.8e4,xra=[0,2000],yra=[0,2000],glob_thresh=0.1):
+    
+    plotfiledir = label+".plots" # no trailing /
+    filpropfile = label+".filprops.pkl" # phys props
+    
+    fits_hdu = fits.open(mom8file)[0]    
+    # setting beamwidth will have no effect if a fits header is given
+    fils=FilFinder2D(fits_hdu, distance=distpc*u.pc)
+    
+    fils.save_name=plotfiledir+"/"+plotfiledir
+    if not os.path.exists(plotfiledir):
+        os.mkdir(plotfiledir)
+        redo=True
+    
+    # give radial profiles more breathing room.
+    #fils.skeleton_pad_size=5
+    
+    fils.preprocess_image(flatten_percent=90)
+    
+    # 30dor: glob=72, flatten=100, adapt=75, size=800
+    # GMC1 1st time: 50,500, 50, 85
+    # ppbm=
+    # can lower glob_thresh, get more small things, then lower adapt_thresh to
+    # make them break up more, and finally let size_thresh kill them
+    # in new version, glob_thresh is no longer a percentage its a value; setting to 50%
+    # of the image max is too high; 20% is closer, 10% quite good
+    fils.create_mask(verbose=True, border_masking=False, 
+                     use_existing_mask=False,save_png=True,
+                     adapt_thresh=50*u.pix, size_thresh=300*u.pix**2,
+                     smooth_size=8*u.pix, glob_thresh=pl.nanmax(fits_hdu.data)*0.1)
+    
+    fils.medskel(verbose=True,save_png=True)
+    
+    print "calling analyze_skeletons"
+    # 30dor:
+    # fils.branch_thresh=200 # typical lengths are 1-100
+    # raise from 10 to 30 to elminate more little things
+    # fils.analyze_skeletons(verbose=True, save_png=True, skel_thresh=10,cubefile=cubefile)
+    
+    # mc3: old version was doing double filter instead, with branch_thresh=30;
+    # branch_thresh says it overrides all previous settings
+    fils.analyze_skeletons(verbose=True, save_png=True, prune_criteria="length",
+                           branch_nbeam_lengths=5,nbeam_lengths=5, skel_thresh=10*u.pix)
+    #relintens_thresh=0.1, branch_thresh=30,
+    
+    # the labeled filament arrays are left separated even though the branches
+    # have been pruned, so we need to recalculate the labelled_filament_arrays
+    # pix_identify is supposed to do that;
+    # fils.filament_arrays comes from make_final_skeletons
+    
+    if redo:
+        pl.clf()
+        vmin = np.percentile(fils.flat_img[np.isfinite(fils.flat_img)], 20)
+        vmax = np.percentile(fils.flat_img[np.isfinite(fils.flat_img)], 99)
+        pl.imshow(fils.flat_img.value, interpolation=None, origin="lower",vmin=vmin, vmax=vmax*1.5, cmap='jet')
+        pl.contour(fils.skeleton, colors='r')
+        offs=fils.array_offsets
+        for i in range(len(offs)):
+            pl.text(0.5*(offs[i][0][1]+offs[i][1][1]),0.5*(offs[i][0][0]+offs[i][1][0]),str(i),color='orange')
+        pl.xlim(xra)
+        pl.ylim(yra)
+        pl.savefig(plotfiledir+"/"+plotfiledir+".skel.png")
+        
+
+        # find_widths calculates the 2d distance transform, and uses self.image, self.imgscale
+        # first widths on mom0
+        fils.set_image(fits.getdata(mom0file))
+        
+        fils.save_name=plotfiledir+".mom0wid"
+        fils.find_widths(verbose=True,save_png=True,max_dist=2.*u.pc)
+        mom0width=fils.width_fits().copy()
+        
+        pl.clf()
+        vmin = np.percentile(fils.image[np.isfinite(fils.image)], 20)
+        vmax = np.percentile(fils.image[np.isfinite(fils.image)], 99)
+        pl.imshow(fils.image.value, interpolation=None, origin="lower",vmin=vmin, vmax=vmax*1.5, cmap='jet')
+        pl.contour(fils.skeleton, colors='r')
+        offs=fils.array_offsets
+        for i in range(len(offs)):
+            pl.text(0.5*(offs[i][0][1]+offs[i][1][1]),0.5*(offs[i][0][0]+offs[i][1][0]),str(i),color='orange')
+        pl.xlim(xra)
+        pl.ylim(yra)
+        pl.savefig(plotfiledir+"/"+plotfiledir+".int.skel.png")
+
+        
+        # now widths and orientations on the mom8 directly:
+        fils.set_image(fits.getdata(mom8file))
+
+        fils.save_name=plotfiledir+".mom8wid"
+        fils.find_widths(verbose=True,save_png=True,max_dist=2.*u.pc) # CHANGE FOR 30DOR
+        mom8width=fils.width_fits().copy()
+        
+        pl.clf()
+        x =fils.converter.from_pixel(mom8width['fwhm'],u.pc).value
+        dx=fils.converter.from_pixel(mom8width['fwhm_err'],u.pc).value
+        y =fils.converter.from_pixel(mom0width['fwhm'],u.pc).value
+        dy=fils.converter.from_pixel(mom0width['fwhm_err'],u.pc).value
+        pl.errorbar(x,y,xerr=dx,yerr=dy,fmt='.')
+        xmax=pl.nanmax([x,y])
+        pl.xlim(0,xmax*1.1)
+        pl.ylim(0,xmax*1.1)
+        pl.xlabel("FWHM fit to mom8 [pc]")
+        pl.ylabel("FWHM fit to mom0 [pc]")
+        pl.plot([0,xmax],[0,xmax],':k')
+        pl.savefig(plotfiledir+"/"+plotfiledir+".widths.mom8.mom0.png")
+     
+    
+    return fils
+
+
+if __name__ == "__main__":
+    distpc=4.8e4
+    label="GMC1_12CO_12m7mT"
+    mom8file = "GMC1_12CO_12m7mTPF.maximum.fits"
+    mom0file = "GMC1_12CO_12m7mTPF.mom0.fits"
+    xra=[180,850]
+    yra=[120,650]
+    glob_thresh=0.1
+    run_filfinder(label=label, mom0file=mom0file, mom8file=mom8file, xra=xra, yra=yra, glob_thresh=glob_thresh, distpc=distpc, redo=True)
